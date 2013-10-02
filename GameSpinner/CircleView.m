f8cbf932fe4f7eb92d46ba32d20de8b0fe018bb6
Matt Simpson <matt.simpson3@gmail.com>
2013-10-01 21:34:30 -0500

    Dynamically adjust the offset for the circle based on the lineWidth

--- GameSpinner/CircleView.m
+++ GameSpinner/CircleView.m
@@ -10,22 +10,40 @@
 
 @interface CircleView ()
 @property (nonatomic) int diameter;
+@property (nonatomic) int lineWidth;
+@property (nonatomic) UIBezierPath *circlePath;
+@property (nonatomic) CGFloat x;
+@property (nonatomic) CGFloat y;
 @end
 
 @implementation CircleView
 - (id)initWithRadius:(int)radius andX:(CGFloat) xCoordinate andY:(CGFloat) yCoordinate{
     self.diameter = radius*2;
-    self = [super initWithFrame:CGRectMake(xCoordinate, yCoordinate, self.diameter, self.diameter)];
+    self.lineWidth = 5;
+    self.x = xCoordinate;
+    self.y = yCoordinate;
+    self = [super initWithFrame:[self rectForFrame]];
     self.backgroundColor = [UIColor clearColor];
+    self.circlePath = [UIBezierPath bezierPathWithOvalInRect:[self rectForCirclePath]];
     return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-    UIBezierPath *aPath = [UIBezierPath bezierPathWithOvalInRect:
-                           CGRectMake(0, 0, self.diameter, self.diameter)];
+- (CGRect) rectForCirclePath{
+    CGFloat width = self.diameter - (self.lineWidth * 2);
+    return CGRectMake(self.lineWidth, self.lineWidth, width, width);
+}
 
+- (CGRect)rectForFrame{
+    return CGRectMake(self.x, self.y, self.diameter, self.diameter);
+}
+
+- (void)drawRect:(CGRect)rect {
+    self.circlePath.lineWidth = self.lineWidth;
+    
+    [[UIColor blackColor] setStroke];
     [[UIColor redColor] setFill];
 
-    [aPath fill];
+    [self.circlePath fill];
+    [self.circlePath stroke];
 }
 @end
